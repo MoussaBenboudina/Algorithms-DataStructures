d2ba65a3529cc2c6261d4be6dfdafd6ac4f210b6
Benboudina Moussa <121502039+MoussaBenboudina@users.noreply.github.com>
2024-10-23 09:47:06 -0700

    add file Sorting

--- algorithms/Sorting/BubbleSort/BubbleSort.cpp
+++ algorithms/Sorting/BubbleSort/BubbleSort.cpp
@@ -1,24 +1,17 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-void swap (int& a , int& b) {
-    int temp = a;
-           a = b;
-           b = temp;
-}
-
-
-
 void BubbleSort(vector<int>& arr) {
-
     const int n = arr.size();
-
-    for(int i = 0; i < n; i++)
-        for(int j = 0;  j < n - i -1; j++)
-            if(arr[j] > arr[j + 1])
+    for(int i = 0; i < n; i++){
+        for(int j = 0;  j < n - i -1; j++) {
+            if(arr[j] > arr[j + 1]) {
                 swap(arr[j] , arr[j + 1]);
-
+            }
+        }
+    }
 }
 
 void printArray(vector<int> arr) {
@@ -31,16 +24,18 @@ void printArray(vector<int> arr) {
 
 int main()
 {
-
     vector<int> arr = {1,4,9,2,0,4,6};
+    
     cout << "before sorting : ";
     printArray(arr);
+    
     BubbleSort(arr);
+
     cout << "after sorting  : ";
     printArray(arr);
 
 
 
-    cout << "Hello world!" << endl;
+
     return 0;
 }
