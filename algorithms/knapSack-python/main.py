2c6fe39d3f3ef6b58b2dbff69b54d3056170b58e
Benboudina Moussa <121502039+MoussaBenboudina@users.noreply.github.com>
2024-11-21 08:11:54 -0800

    add greedy_by_ratio

--- algorithms/knapSack-python/main.py
+++ algorithms/knapSack-python/main.py
@@ -1,13 +1,15 @@
 
 def isValid(weights, x, W):
-    if W <= 0: return False
-    n = len(weights)
-    sumWeight = 0
-    for i in range(0, n):
+
+    if W <= 0: return False 
+
+    n = len(weights) 
+    sumWeight = 0 
+    for i in range(0, n): 
         sumWeight += weights[i] * x[i]
         if sumWeight > W:
-            return False
-    return sumWeight <= W
+            return False 
+    return sumWeight <= W 
 
 
 
@@ -21,28 +23,63 @@ def fitness(values, x):
 
 
 
-
 def greedy_by_value(weight , values , x , W) :
-  
-  sorted_pairs = sorted(zip(values, weights), reverse=True)
-  values_sorted, weights_sorted = zip(*sorted_pairs)
-  values_sorted = list(values_sorted)
-  weights_sorted = list(weights_sorted)
 
-  n = len(weight)
-  max_value = 0;
-  for i in range(0 , n) : 
+    if len(weight) == 0 or len(values) == 0 or W <= 0: 
+       print('invalid input: weight, values cannot be empty, and W must be greater than 0.')
+       return;
+ 
+    n = len(weight)
+    max_value = 0;
+
+  
+    sorted_pairs = sorted(zip(values, weights), reverse=True)
+    values_sorted, weights_sorted = zip(*sorted_pairs)
+    values_sorted = list(values_sorted)
+    weights_sorted = list(weights_sorted)
+
+    for i in range(0 , n) : 
       x[i] = 1;
       if not isValid(weights_sorted , x, W) :
           x[i] = 0;
       else :
           max_value += values_sorted[i]
 
-  print(x)
-  return x , max_value;
+    print(x)
+    return x , max_value;
 
 
 
+
+def greedy_by_ratio (weight , values , x , W) :
+
+    if len(weight) == 0 or len(values) == 0 or W <= 0: 
+        print('invalid input: weight, values cannot be empty, and W must be greater than 0.')
+        return;
+
+    n = len(weight);
+    max_value = 0;
+    
+    ratio = []
+    for i in range(0 , n) :
+        ratio.append(values[i] / weight[i]);
+    
+    sorted_pairs = sorted(zip(values, weights , ratio),key=lambda x : x[2] , reverse=True);
+    values_sorted , weight_sorted , ratio_sorted = zip(*sorted_pairs);
+    values_sorted , weight_sorted , ratio_sorted =   list(values_sorted) , list(weight_sorted) , list(ratio_sorted) 
+    print("values_sorted :" , values_sorted , "\nweight_sorted  : " , weight_sorted  , "\nratio_sorted : " , ratio_sorted ) 
+    for i in range(0 , n) :
+        x[i] = 1;
+        if not isValid(weights , x , W) :
+            x[i] = 0;
+        else :
+            max_value += values_sorted[i];
+
+    
+    return  x , max_value;
+
+
+    
 
 def generate_random_solution(weights , values , W):
     import random
@@ -80,15 +117,13 @@ def Exhaustive(weights, values, W):
 
 
 
-
-
 try:
-    weights = [10, 35, 23, 20]
-    values = [10, 20, 40, 30]
+    weights = [10, 35, 23, 20 , 50 ,15 , 24 , 12 , 4]
+    values = [10, 20, 40, 30 , 60 , 20 , 30 , 25 , 20]
     x = [0] * len(values)
-    W = 50
+    W = 100
 
-    print('_' * 100, "\nExhaustive")
+    print('_' * 100, "\nExhaustive (optimal) :")
     best_solution, max_value = Exhaustive(weights, values, W)
     print("best solution:", best_solution, "\nmax value:", max_value)
 
@@ -96,11 +131,15 @@ try:
     best_solution, max_value = greedy_by_value(weights, values, x, W)
     print("best solution:", best_solution, "\nmax value:", max_value)
 
+    print('_' * 100 , "\n:Greedy by ration")
+    best_solution, max_value = greedy_by_ratio(weights, values, x, W)
+    print("best solution:", best_solution, "\nmax value:", max_value)
+
     print('_' * 100 , "\nRandom binary generation :")
     val , solution = generate_random_solution(weights, values)
     print("solution:", solution, "\nvalue:", val)
 
-    
+
 
 except Exception as e:
     print("An error occurred:", e)
